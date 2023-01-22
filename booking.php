cdf6376ca92edc1992c8fa1ae686b539eb1c7728
prodananastasiia <n.prodan3@gmail.com>
2023-01-22 20:12:34 +0100

    first commit

---
+++ booking.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+    Check-in <?php echo $_POST["checkin"]; ?>
+    Check-out <?php echo $_POST["checkout"]; ?>
+    Adults <?php echo $_POST["total_adults"]; ?>
+    Children <?php echo $_POST["total_children"]; ?>
+
+</body>
+</html>   
